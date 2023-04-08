a3c760de2474bfb5b97424866e8ac3401f239143
Leopold Lemmermann <leopold@lemmermann.com>
2023-04-08 16:31:57 +1200

    Implemented the student response type.

--- src/main/java/studentscroll/api/students/web/responseDTO/StudentResponse.java
+++ src/main/java/studentscroll/api/students/web/responseDTO/StudentResponse.java
@@ -1,5 +1,12 @@
 package studentscroll.api.students.web.responseDTO;
 
+import lombok.Data;
+
+@Data
 public class StudentResponse {
+
+  private Long id;
+  private String name;
+  private String email;
 
 }
