c009d8b2ff1e36dc6b382bff245b62629b268082
Leopold Lemmermann <leopold@lemmermann.com>
2023-04-08 13:03:49 +1200

    Refactored the JWTUtils into a JSONWebToken class.

---
+++ src/main/java/studentscroll/api/security/JSONWebToken.java
@@ -0,0 +1,62 @@
+package studentscroll.api.security;
+
+import java.security.Key;
+import java.util.*;
+import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+@SuppressWarnings("JavaUtilDate")
+public class JSONWebToken {
+
+  public static final long EXPIRES_AFTER_SECONDS = 24 * 60 * 60;
+  private static final Key KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+  private String token;
+
+  public JSONWebToken(String token) {
+    this.token = token;
+  }
+
+  public static JSONWebToken generateFrom(UserDetails details) {
+    return new JSONWebToken(
+        Jwts.builder()
+            .setClaims(new HashMap<String, Object>())
+            .setSubject(details.getUsername())
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + EXPIRES_AFTER_SECONDS * 1000))
+            .signWith(KEY)
+            .compact());
+  }
+
+  public Boolean validate(UserDetails details) {
+    return (getUsername().equals(details.getUsername())) && !isExpired();
+  }
+
+  public String getUsername() {
+    return getClaim(Claims::getSubject);
+  }
+
+  public Date getExpirationDate() {
+    return getClaim(Claims::getExpiration);
+  }
+
+  public Boolean isExpired() {
+    return getExpirationDate().before(new Date());
+  }
+
+  private <T> T getClaim(Function<Claims, T> resolve) {
+    return resolve.apply(getAllClaims());
+  }
+
+  private Claims getAllClaims() {
+    return Jwts.parserBuilder()
+        .setSigningKey(KEY).build()
+        .parseClaimsJws(token).getBody();
+  }
+
+}
---
+++ src/test/java/studentscroll/api/unit/JSONWebTokenTests.java
@@ -0,0 +1,53 @@
+package studentscroll.api.unit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+
+import lombok.val;
+import studentscroll.api.security.JSONWebToken;
+import studentscroll.api.students.data.Student;
+
+public class JSONWebTokenTests {
+
+  @Test
+  public void givenUserDetailsAreValid_whenGeneratingJWT_thenReturnsJWTWithCorrectEmail() {
+    val email = "abc@xyz.com";
+    val details = Student.builder().email(email).build();
+    val token = JSONWebToken.generateFrom(details);
+
+    assertEquals(email, token.getUsername());
+  }
+
+  @Test
+  public void givenJWTIsCreated_whenGettingExpirationDate_thenDateReflectsExpirationInterval() {
+    val details = Student.builder().email("xxx").build();
+    val token = JSONWebToken.generateFrom(details);
+
+    val expirationInterval = JSONWebToken.EXPIRES_AFTER_SECONDS;
+    val expirationDate = token.getExpirationDate();
+
+    // slight adjustment because current date will be later than JWT creation date
+    assertTrue(expirationDate.after(new Date(System.currentTimeMillis() - 9999 + expirationInterval * 1000)));
+  }
+
+  @Test
+  public void givenJWTIsValid_whenValidatingJWT_thenReturnsTrue() {
+    val details = Student.builder().email("xxx").build();
+    val token = JSONWebToken.generateFrom(details);
+
+    assertTrue(token.validate(details));
+  }
+
+  @Test
+  public void givenJWTIsInvalid_whenValidatingJWT_thenReturnsFalse() {
+    val details = Student.builder().email("xxx").build();
+    val otherDetails = Student.builder().email("yyy").build();
+    val token = JSONWebToken.generateFrom(details);
+
+    assertFalse(token.validate(otherDetails));
+  }
+
+}
