17ee3a67742179d08bc37ebd38eabcfeed45ff63
Leopold Lemmermann <leopold@lemmermann.com>
2023-04-08 16:30:37 +1200

    Implemented the create student request.

--- src/main/java/studentscroll/api/students/web/requestDTO/CreateStudentRequest.java
+++ src/main/java/studentscroll/api/students/web/requestDTO/CreateStudentRequest.java
@@ -1,5 +1,12 @@
 package studentscroll.api.students.web.requestDTO;
 
+import lombok.Data;
+
+@Data
 public class CreateStudentRequest {
+
+  private String name;
+  private String email;
+  private String password;
 
 }
