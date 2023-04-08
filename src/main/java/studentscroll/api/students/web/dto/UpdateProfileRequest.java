5f49e6ded506766df8fa955e7b31922afe146696
Leopold Lemmermann <leopold@lemmermann.com>
2023-04-09 09:47:28 +1200

    Removed optionals from web layer.

--- src/main/java/studentscroll/api/students/web/dto/UpdateProfileRequest.java
+++ src/main/java/studentscroll/api/students/web/dto/UpdateProfileRequest.java
@@ -1,16 +1,15 @@
 package studentscroll.api.students.web.dto;
 
-import java.util.*;
 import lombok.Data;
 import studentscroll.api.shared.Location;
 
 @Data
 public class UpdateProfileRequest {
 
-  private Optional<String> name;
-  private Optional<String> bio;
-  private Optional<String> icon;
-  private Optional<String[]> interests;
-  private Optional<Location> location;
+  private String name;
+  private String bio;
+  private String icon;
+  private String[] interests;
+  private Location location;
 
 }
