c199db90a7327616e42935326da9af3e16bf26a7
Leopold Lemmermann <leopold@lemmermann.com>
2023-04-09 19:57:34 +1200

    Implemented creating and updating in the event post service.

--- src/main/java/studentscroll/api/posts/data/PostRepository.java
+++ src/main/java/studentscroll/api/posts/data/PostRepository.java
@@ -1,19 +1,7 @@
 package studentscroll.api.posts.data;
 
-import java.util.Set;
-
 import org.springframework.data.repository.CrudRepository;
 
-import lombok.NonNull;
-
 public interface PostRepository extends CrudRepository<Post, Long> {
-
-  public Set<Post> findByPosterId(@NonNull Long posterId);
-
-  public Set<Post> findByTitle(@NonNull String title);
-
-  public Set<Post> findDistinctByTagsIn(@NonNull Set<String> tags);
-
-  public Set<Post> findFirst10();
 
 }
