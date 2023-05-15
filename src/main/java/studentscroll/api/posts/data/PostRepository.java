e2343bfb526a6c0603a54a65c1af18812936d738
Leopold Lemmermann <leopold@lemmermann.com>
2023-05-15 17:49:06 +1200

    Reading posts now returns posts by follows preferredly.

--- src/main/java/studentscroll/api/posts/data/PostRepository.java
+++ src/main/java/studentscroll/api/posts/data/PostRepository.java
@@ -1,11 +1,13 @@
 package studentscroll.api.posts.data;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-  Page<Post> findByPosterId(Long id, Pageable page);
+  Page<Post> findByPosterIdIn(List<Long> posterIds, Pageable page);
 
   Page<Post> findByTags(String tag, Pageable page);
 
