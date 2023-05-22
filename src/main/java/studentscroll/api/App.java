5681a8ccce33c9e519175f5e4bae0a887ffd38e0
Leopold Lemmermann <leopold@lemmermann.com>
2023-05-22 23:24:34 +1200

    Updated the docs version and removed the production config.

--- src/main/java/studentscroll/api/App.java
+++ src/main/java/studentscroll/api/App.java
@@ -7,8 +7,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.*;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition(info = @Info(version = "1.0.0", title = "Student Scroll REST API", description = "This is the REST API for Student Scroll.", license = @License(name = "MIT License", url = "https://github.com/Leo-Lem/StudentScrollAPI/blob/main/LICENSE")), servers = {
-    @Server(url = "https://studentscroll.net/api/v1", description = "production"),
+@OpenAPIDefinition(info = @Info(version = "2.0", title = "Student Scroll REST API", description = "This is the REST API for Student Scroll.", license = @License(name = "MIT License", url = "https://github.com/Leo-Lem/StudentScrollAPI/blob/main/LICENSE")), servers = {
+    @Server(url = "https://studentscroll.net/api/v2", description = "production"),
     @Server(url = "http://localhost:8080", description = "development") })
 @SpringBootApplication
 public class App {
